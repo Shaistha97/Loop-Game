de3e1bdd1c06043821340e843fd38c16832a205d
Shaistha <nayeemashariff26@gmail.com>
2018-06-23 18:30:00 +0530

    adding project

---
+++ app/src/main/java/com/example/shaistha/loop/Constants.java
@@ -0,0 +1,11 @@
+package com.example.shaistha.loop;
+
+import android.content.Context;
+
+public class Constants
+{
+    public static long INIT_TIME;
+    public static int SCREEN_HEIGHT;
+    public static int SCREEN_WIDTH;
+    public static Context CURRENT_CONTEXT;
+}
